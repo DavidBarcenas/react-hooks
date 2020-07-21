fcf2356203b5b6a305d20ac31caff7cb4574873e
David Barcenas <daveepro@outlook.com>
2020-07-21 14:30:28 -0500

    style: Create todoApp layout

--- src/components/useReducer/Todo.js
+++ src/components/useReducer/Todo.js
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react'
 import { todoReducer } from './todoReducer'
+import './useReducer.css'
 
 const initialState = [{
   id: new Date().getTime(),
@@ -13,18 +14,27 @@ export const Todo = () => {
 
   return (
     <div className="text-center p-5">
-      <h1 className="text-primary">Todo App</h1>
+      <h1 className="text-primary">Todo App ({todos.length})</h1>
       <hr/>
+
+      {/* Form */}
+
+      <form className="mb-3">
+        <input type="text" name="desc" placeholder="ToDo "/>
+        <button className="btn btn-success btn-sm mb-1">Add ToDo</button>
+      </form>
 
       <ul className="list-group text-left">
         {
-          todos.map(todo => (
-          <li key={todo.id} className="list-group-item">
-            <label className="m-0">{todo.desc}</label>
+          todos.map((todo, i) => (
+          <li key={todo.id} className="list-group-item d-flex justify-content-between">
+            <label className="m-0">{i + 1} - {todo.desc}</label>
+            <button className="btn btn-danger btn-sm">X</button>
           </li>
           ))
         }
       </ul>
+
     </div>
   )
 }
